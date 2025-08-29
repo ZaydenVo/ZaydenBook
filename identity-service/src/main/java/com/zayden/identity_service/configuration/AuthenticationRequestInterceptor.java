0bd046efcd29b7aebfe3a0afc1e86ac98d21106c
Zayden Vo Tran Tin <votrantin@gmail.com>
2025-08-29 09:56:49 +0700

    Notification service

--- identity-service/src/main/java/com/zayden/identity_service/configuration/AuthenticationRequestInterceptor.java
+++ identity-service/src/main/java/com/zayden/identity_service/configuration/AuthenticationRequestInterceptor.java
@@ -3,15 +3,11 @@ package com.zayden.identity_service.configuration;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.util.Objects;
-
 @Slf4j
-@Component
 public class AuthenticationRequestInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
--- profile-service/src/main/java/com/zayden/profile/configuration/SecurityConfig.java
+++ profile-service/src/main/java/com/zayden/profile/configuration/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final String[] PUBLIC_ENDPOINTS = {
-
+            "/internal/users"
     };
 
     private CustomJwtDecoder customJwtDecoder;
