0e2b3108910e971ee115fce99f860ffab286a0e5
Zayden Vo Tran Tin <votrantin@gmail.com>
2025-08-30 16:29:17 +0700

    Post service update.

---
+++ identity-service/target/classes/ApplicationInitConfig.java
@@ -0,0 +1,60 @@
+package com.zayden.identity.configuration;
+
+import java.util.HashSet;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.zayden.identity.entity.Role;
+import com.zayden.identity.entity.User;
+import com.zayden.identity.repository.RoleRepository;
+import com.zayden.identity.repository.UserRepository;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class ApplicationInitConfig {
+
+    PasswordEncoder passwordEncoder;
+    RoleRepository roleRepository;
+
+    @Bean
+    @ConditionalOnProperty(
+            prefix = "spring",
+            value = "datasource.driverClassName",
+            havingValue = "com.mysql.cj.jdbc.Driver")
+    ApplicationRunner applicationRunner(UserRepository userRepository) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+                var roles = new HashSet<Role>();
+
+                Role adminRole = roleRepository.findById("ADMIN").orElseGet(() -> {
+                    Role role = Role.builder()
+                            .name("ADMIN")
+                            .description("Admin role")
+                            .build();
+                    return roleRepository.save(role);
+                });
+                roles.add(adminRole);
+
+                User user = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .roles(roles)
+                        .build();
+
+                userRepository.save(user);
+                log.warn("Admin user has been created with default password: admin, please change it!");
+            }
+        };
+    }
+}
